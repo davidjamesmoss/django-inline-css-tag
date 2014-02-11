5eb4be43dc96d4171bc37e8a6d32ae2c55c3980b
David Moss <david@kentonfrank.com>
2014-02-11 11:06:39 +0000

    Updated for Pynliner 0.5.0; removed encoding and updated readme.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Django template tag for creating inline CSS using Pynliner
+# A Django template tag for creating inline CSS
 
 **Package name: inline_css**
 
-Django template tag to generate inline CSS for any HTML it surrounds. 
+A simple wrapper around [Pynliner](https://github.com/rennat/pynliner) to generate inline CSS in a Django template. Ideal for sending HTML email.
 
 
 ### Installation
@@ -12,19 +12,19 @@ Add 'inline_css' to INSTALLED_APPS
 
 ### Requirements
 
-- [Pynliner](https://github.com/rennat/pynliner)
+- [Pynliner](https://github.com/rennat/pynliner) >= 0.5.0
 
 
 ### Usage
-	
+
 	{% load inline_css %}
-	{% inline_css %} 
+	{% inline_css %}
 		<style>h1 { color:#ffcc00; }</style>
 		<h1>Hello World!</h1>
-	{% endinline_css %} 
+	{% endinline_css %}
 
 
 ### Output
-	
+
 	<h1 style="color: #fc0">Hello World!</h1>
-	
+
--- inline_css/templatetags/inline_css.py
+++ inline_css/templatetags/inline_css.py
@@ -1,25 +1,24 @@
-# -*- coding: utf-8 -*-
-# By David Moss
-# Requires Pynliner https://github.com/rennat/pynliner
-
 from django.template import Library
 from django import template
 import pynliner
 
+
 register = Library()
+
 
 @register.filter
 def do_inline_css(parser, token):
-	nodelist = parser.parse(('endinline_css',))
-	parser.delete_first_token()
-	return InlineCSSNode(nodelist)
+    nodelist = parser.parse(('endinline_css',))
+    parser.delete_first_token()
+    return InlineCSSNode(nodelist)
+
 
 class InlineCSSNode(template.Node):
-	def __init__(self, nodelist):
-		self.nodelist = nodelist
-	def render(self, context):
-		# Encode entities to ascii &#000; style as Pynliner seems to choke otherwise
-		output = self.nodelist.render(context).encode('ascii', 'xmlcharrefreplace')
-		return pynliner.Pynliner().from_string(output).run()
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        output = self.nodelist.render(context)
+        return pynliner.Pynliner().from_string(output).run()
 
 inline_css = register.tag("inline_css", do_inline_css)
